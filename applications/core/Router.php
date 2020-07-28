e54579c07106648e56e0ca1ad0535ce34e69f25d
Teron-comm <68468485+Teron-comm@users.noreply.github.com>
2020-07-28 15:31:17 +0300

    dev

---
+++ applications/core/Router.php
@@ -0,0 +1,10 @@
+<?php
+namespace applications\core;
+
+class Router 
+{
+    function __construct()
+    {
+        echo 'я класс рутер';
+    }
+}
--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,inital-sacle=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-      crossorigin="anonymous"
-    />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
     <title>SITEFLOW</title>
-  </head> 
+</head>
+
 <body>
-<?php 
-require ('applications/lib/dev.php');
-debug($test);
-?>
-qwe
+    <?php
+    require('applications/lib/dev.php');
+
+    use applications\core\Router;
+
+    spl_autoload_register(function ($class) {
+        $path = str_replace('\\', '/', $class . '.php');
+        if (file_exists($path)) {
+            require $path;
+        }
+    });
 
 
 
 
+    $rounter = new Router;
+session_start();
+
+    ?>
 
 
 
 
 
 </body>
+
 </html>
